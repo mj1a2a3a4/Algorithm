a0525249fe0cbfe7fd6e09492fa7490eaf184dbf
MIN JOON KIM <104118005+mj1a2a3a4@users.noreply.github.com>
2022-10-29 23:00:55 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.30 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int piece = (int)Math.ceil(n/7.0);
+        answer = piece;
+        return answer;
+        
+        
+    }
+}
