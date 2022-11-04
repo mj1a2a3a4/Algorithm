4be67cb883447ba68ea0717707a3641f8fc07814
MIN JOON KIM <104118005+mj1a2a3a4@users.noreply.github.com>
2022-11-04 19:51:35 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.38 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,13 @@
+import java.util.Arrays; 
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int[] max = numbers;
+
+        Arrays.sort(max);   // 배열 자체를 정렬된 순서로 변경
+
+        answer = max[max.length - 1] * max[max.length - 2];
+
+        return answer;
+    }
+}
